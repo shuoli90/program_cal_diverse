69e83456dd4403e186f5191d779a3d8f8a90c9e0
shuoli90 <lishuo1@seas.upenn.edu>
2024-02-09 12:31:54 -0500

    update readme

--- README.md
+++ README.md
@@ -8,3 +8,9 @@ We structure the implementation by functionality. Here is a brief description of
 - metrics: this folder contains implementations for generation correctness, calibration and diversity metrics.
 - utils: this folder contains miscellaneous functions.
 - run: this folder contains scripts that are supposed to be run by users. Functions include generating responses, calibrating LLM using a specific indicator, comparing multiple indicators.
+
+## Simple utilization
+'''
+cd run
+python generate_code.py --prompt 'Generate a function to calculate the factorial of a number.' [--temperature TEMPERATURE] [--max_length LENGTH] [--num_return_sequences NUM_GENERATIONS]
+'''
--- run/generate_code.py
+++ run/generate_code.py
@@ -1,17 +1,20 @@
 import os
 import sys
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-import torch
 import argparse
 from models import opensource, gpt
 from utils import templates
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '..')))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='Generate text from a prompt')
-    parser.add_argument('--model', type=str, default='codellama/CodeLlama-7b-hf', 
+    parser.add_argument('--model', type=str,
+                        default='codellama/CodeLlama-7b-hf',
                         help='model to use')
     parser.add_argument('--prompt', type=str, default='Once upon a time')
     parser.add_argument('--template', type=str, default='vanilla')
+    parser.add_argument('--temperature', type=float, default=0.7)
     parser.add_argument('--max_length', type=int, default=50)
     parser.add_argument('--num_return_sequences', type=int, default=1)
     args = parser.parse_args()
@@ -21,11 +24,14 @@ if __name__ == '__main__':
         pipe = gpt.GPTModel(model_name=args.model)
     else:
         pipe = opensource.OpensourceModel(model_name=args.model)
-    
+
     if args.template == 'vanilla':
         prompt = templates.vanilla_template(args.prompt)
     else:
         raise ValueError("Unknow template")
-    
+
     # Generate text
-    generated = pipe.generate(args.prompt, max_length=args.max_length, num_return_sequences=args.num_return_sequences, return_full_text=False)
+    generated = pipe.generate(args.prompt, temperature=args.temperature,
+                              max_length=args.max_length,
+                              num_return_sequences=args.num_return_sequences,
+                              return_full_text=False)

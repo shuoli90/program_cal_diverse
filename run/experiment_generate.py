0e4e904a8d53e3ed802f74a80fe02f3387af719b
Alex Shypula <ags417@stern.nyu.edu>
2024-06-19 02:15:25 -0400

    add the generation script

---
+++ run/experiment_generate.py
@@ -0,0 +1,221 @@
+import os
+import sys
+sys.path.insert(0,
+                os.path.abspath(os.path.join(os.path.dirname(__file__),
+                                             '..')))
+import argparse
+import pandas as pd
+import json
+import numpy as np
+from models import gpt, opensource, hf_inference
+from utils import textprocessing
+from utils.clustering import clustering
+from utils.clustering.clustering import tqdm_joblib
+import joblib
+from joblib import Parallel, delayed
+from utils.clustering import lexical_diversity
+from utils.clustering.ast_processing import AllSubtreeAnalysis, AstSubTree, parallel_subtree_analysis
+from dataclasses import dataclass
+import yaml
+from tqdm import tqdm
+from functools import partial
+import datetime
+import traceback
+
+import signal
+import traceback
+
+
+
+def handler(pipe, signum, frame):
+    print("Signal received, handling cleanup...")
+    try:
+        print("Stopping and removing the service...")
+        pipe.stop_service()
+        pipe.remove_service()
+        print("Cleanup complete, exiting...")
+    except Exception as e:
+        print("Error during cleanup:", e)
+        traceback.print_exc()
+    finally:
+        sys.exit(0)  # Ensure the process exits
+
+from functools import partial
+
+# def partial_handler(pipe, args): 
+#     return partial(handler, pipe, args)
+
+
+@dataclass
+class Arguments:
+    experiment_string: str
+    path_to_dataset: str = '../data/open_ended/open_ended_final/dataset.jsonl'
+    experiment_output_root: str = '../collected/'
+    model: str = 'gpt-3.5-turbo'
+    template: str = 'open_ended_default'
+    temperature: float = 1.0
+    top_p: float = 1.0
+    max_length: int = 768
+    num_return_sequences: int = 10
+    repetition_penalty: float = 1.0
+    parallel_samples: int = 5
+    port: int = 9999
+    devices_list: str = '4,5,6,7'
+    startup_timeout: int = 600
+    generation_timeout: int = 100
+    volume: str = 'saved_models'
+    path_to_hf_token: str = None
+    batch_size: int = None
+    max_programs: int = -1
+    
+    
+template_dir = os.path.join(os.path.dirname(__file__), "../prompt_templates")
+templates = [f for f in os.listdir(template_dir) if f.endswith('.txt')]
+template_names = [f.split('.')[0] for f in templates] + [None]
+    
+def readin_template(template_arg): 
+    assert template_arg in template_names, f"Template {template_arg} not found. Available templates: {template_names}"
+    if template_arg == None: 
+        return "## DESCRIPTION\n"
+    else: 
+        path_to_template = os.path.join(os.path.dirname(__file__), f"../prompt_templates/{template_arg}.txt")
+        with open(path_to_template, 'r') as file:
+            template = file.read()
+        return template
+    
+
+def _format_template(prompt, template: str): 
+    formatted_prompt = template.replace("## DESCRIPTION", prompt)
+    assert "## DESCRIPTION" not in formatted_prompt, "Template not formatted correctly"
+    return formatted_prompt
+
+
+def load_arguments_from_yaml(yaml_file):
+    with open(yaml_file, 'r') as file:
+        args_dict = yaml.safe_load(file)
+    return Arguments(**args_dict)
+
+
+if __name__ == '__main__':
+    path_to_yaml = sys.argv[1]
+    args = load_arguments_from_yaml(path_to_yaml)
+    
+    # make sure the template is valid
+    prompt_template = readin_template(args.template)
+    format_template_fun = partial(_format_template, template=prompt_template)
+    
+    model_name_clean = args.model.replace("/", "-")
+    # experiment_string = f"{model_name_clean}_temp_{args.temperature}_top_p_{args.top_p}_max_length_{args.max_length}_num_return_sequences_{args.num_return_sequences}_repetition_penalty_{args.repetition_penalty}_{args.template}_{datetime.datetime.now().strftime('%Y-%m-%d_%H-%M-%S')}"
+    experiment_string = args.experiment_string
+    experiment_output_dir = os.path.join(args.experiment_output_root, experiment_string)
+        
+    os.makedirs(experiment_output_dir, exist_ok=False) # there should be no existing directory (H-M)
+    
+    # save config
+    with open(os.path.join(experiment_output_dir, 'config.yaml'), 'w') as f:
+        yaml.dump(args.__dict__, f)
+    
+    # Setup generation pipeline
+    if 'gpt' in args.model or 'davinci' in args.model:
+        pipe = gpt.GPTModel(model_name=args.model)
+    else:
+        # pipe = opensource.OpensourceModel(model_name=args.model)
+        with open(args.path_to_hf_token, "r") as f:
+            hf_key = f.read().strip()
+        pipe = hf_inference.HFInferenceService(model_name=args.model, 
+                                                parallel_samples=max(args.parallel_samples,args.num_return_sequences),
+                                                port=args.port,
+                                                devices_list=args.devices_list,
+                                                volume=args.volume,
+                                                startup_timeout=args.startup_timeout,
+                                                generation_timeout=args.generation_timeout,
+                                                hf_key=hf_key)
+        # sigint_handler = partial_handler(pipe)
+        sigint_handler = partial(handler, pipe)
+        signal.signal(signal.SIGINT, sigint_handler)
+    try:                                                
+
+        # Read in data
+        print(f'reading in data from {args.path_to_dataset}')
+        df = pd.read_json(args.path_to_dataset, lines=True, orient='records')
+        # get first 5 rows
+        if args.max_programs > 0:
+            df = df.iloc[:args.max_programs]
+
+        # setup docker client
+        client, image = clustering.build_docker_image(clustering.clustering_abs_dir)
+
+        results = []
+        count = 0
+        # iterate over the dataframe
+        for index, row in tqdm(df.iterrows()):
+            # try:
+            result = {}
+            result['model'] = args.model
+            result['index'] = index
+            
+            prompt = row['description_string']
+            problem_id = row['problem_id']
+            extract_arguments_fun = row["extract_args_fun"]
+            
+            # store the row info into result
+            result.update(row.to_dict())
+            
+            # format the prompt
+            formatted_prompt = format_template_fun(prompt)
+            result['formatted_prompt'] = formatted_prompt
+            
+            # Generate text
+            generateds_program = pipe.generate(
+                formatted_prompt, 
+                temperature=args.temperature,
+                num_return_sequences=args.num_return_sequences,
+                # TODO: add more args
+                max_length=args.max_length,
+                do_sample=True, 
+                return_dict_in_generate=True, 
+                batch_size=args.batch_size,
+            )
+            
+            programs = [textprocessing.extract_python_code(g) for g in generateds_program]
+            formatted_programs = [clustering.format_open_ended_code(program, extract_arguments_fun) for program in programs]
+            
+    
+            result['description'] = prompt
+            result['programs'] = programs
+            result['formatted_programs'] = formatted_programs
+            testcase_inputs = row['input_testcases']
+            result['input_testcases'] = testcase_inputs
+    
+                                                                                   
+            problem_id_dir = os.path.join(experiment_output_dir, f'problem_{problem_id}')   
+            problem_id_gen_dir = os.path.join(problem_id_dir, 'generated')
+            os.makedirs(problem_id_gen_dir, exist_ok=False)                 
+            for i, (generation, program, formatted_program) in enumerate(zip(generateds_program, programs, formatted_programs)):
+                with open(os.path.join(problem_id_gen_dir, f'gen_{i}_coh_.txt'), 'w') as f:
+                    f.write(generation)
+                with open(os.path.join(problem_id_gen_dir, f'prog_{i}_coh.txt'), 'w') as f:
+                    f.write(program)
+                with open(os.path.join(problem_id_gen_dir, f'formatted_prog_{i}.txt'), 'w') as f:
+                    f.write(formatted_program)
+                    
+            results.append(result)
+            
+
+        # save results to jsonl
+        with open(os.path.join(experiment_output_dir, 'results.jsonl'), 'w') as f:
+            for result in results:
+                f.write(json.dumps(result) + '\n')
+            
+        if "gpt" not in args.model:
+            pipe.stop_service()
+            pipe.remove_service()
+        
+        print('Done')
+        
+    except Exception as e:
+        traceback.print_exc()
+        if "gpt" not in args.model:
+            pipe.stop_service()
+            pipe.remove_service()
+        raise e
